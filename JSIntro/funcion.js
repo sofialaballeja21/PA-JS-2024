0163330aa1eb2f9a7aa823c60422747d4e7a697a
sofialaballeja21 <sofialabellaja21@gmail.com>
2024-08-27 10:16:02 -0300

    Carpeta de JSIntro

---
+++ JSIntro/funcion.js
@@ -0,0 +1,17 @@
+//Funtion declaration
+
+function saludar(nombre) {
+    console.log('Bienvenido '+ nombre);
+}
+saludar('Jose');
+
+const cliente =function(nombreClientte) {
+    console.log('Mostrando datos del cliente: '+ nombreClientte );
+}
+cliente('Jose')
+
+function actividad(nombre, actividad){
+    console.log('La persona ${nombre} , esta realizando la actividad ${actividad}');
+}
+actividad('Jose', 'Aprendiendo JS');
+
